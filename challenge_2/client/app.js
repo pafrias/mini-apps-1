d719e8e61680d4e5d86c3bf6d29e94182f68d4d2
Pablo A Frias <pafrias90@gmail.com>
2019-01-29 08:55:40 -0800

    Create basic server structure for challenge 2
    Imported express, lodash, $, and nodemon for file system. Skeletal
    structure only, no real functionality.

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,13 @@
+//import request?
 
+// array to save server data
+// F() to display data
+// cb F() to add single data to array
+// ** and/or to manually append to screen
 
+/*
+
+  GET request to get all form data from server with callback
+  POST request to post data to server with callback
+
+*/
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,11 @@
+var app = require('express');
+var _ = require('lodash');
+const PORT = 8080;
 
+var controls = require('./router/controls.js')
 
+/*
+  * create the server with express, with router controllers
+  * create static page relationship
+  * listen to PORT
+//
