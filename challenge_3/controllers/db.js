2c3c153d1625381e6e722c5b148566989ca65ab9
Hack Reactor Student <student@hackreactor.com>
2019-01-30 17:23:54 -0800

    Implement functional SQL db and Express server request handlers

--- challenge_3/controllers/db.js
+++ challenge_3/controllers/db.js
@@ -1,9 +1,70 @@
-var db = require('mysql');
+var mysql = require('mysql');
 
-db.createConnection({
-  //user
-  //password
-  //dbname
-})
+var db = mysql.createConnection({
+  host: 'localhost',
+  user: 'student',
+  password: 'student',
+  database: 'glamazon_prime'
+});
 
-exports.modules = db;
+// var connectAsync = () => {
+//   return new Promise((resolve, reject) => {
+//     db.connect((err, data) => {
+//       if (err) {
+//         console.log(err);
+//         console.log('failed to talk to database');
+//         reject(err);
+//       } else {
+//         console.log('spoke with database');
+//         resolve(data);
+//       }
+//     })
+//   })
+// }
+
+module.exports.read = callback => {
+
+  var sql = 'select * from customers';
+
+  return new Promise((resolve, reject) => {
+    db.query(sql, (err, data) => {
+      if (err) {
+        console.log(err);
+        console.log('failed to query to database');
+        reject(err);
+      } else {
+        console.log('successfully queried database');
+        resolve(data);
+      }
+    })
+  })
+}
+
+module.exports.create = (tableName, callback) => {
+
+  return new Promise((resolve, reject) => {
+    db.query(sql, (err, data) => {
+      if (err) {
+        console.log(err);
+        console.log('failed to query to database');
+        reject(err);
+      } else {
+        console.log('successfully queried database');
+        resolve(data);
+      }
+    })
+  })
+}
+
+/*
+
+COPY-PASTA CONSIDER REFORMATING
+
+function getUserDataAsync(userId) {
+    return new Promise(function(resolve, reject) {
+        // Put all your code here, this section is throw-safe.
+        getUserData(userId, resolve, reject);
+    });
+}
+
+*/
