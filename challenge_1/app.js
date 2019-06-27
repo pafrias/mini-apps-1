f2bd7cc56850c755d787b132991d5d2bf97a082c
Pablo Frias <pafrias90@gmail.com>
2019-06-27 16:11:06 -0700

    Clean up TicTacToe functions, add jsDoc function comments

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,70 +1,73 @@
-const body = window.document.body;
-const gameBoard = document.getElementById('game-board');
 
 //model of board
 class TicTacToeBoard {
   constructor() {
+    this.clickHandler = this.clickHandler.bind(this);
+    this.gameOver = this.gameOver.bind(this);
+    this.boardNode = document.getElementById('game_board');
+  }
+  
+  /**
+   * @description renders the Tic Tac Toe Board to the DOM
+   */
+  resetBoard() {
+    this.playerOneTurn = true;
     this.board = [
       [null, null, null], 
       [null, null, null], 
       [null, null, null]
-    ],
-    this.playerOneTurn = true;
-    this.gameOver = false;
-    this.toggle = this.toggle.bind(this);
-  }
-  
-  displayBoard() {
-    gameBoard.innerHTML = '';
+    ];
+    this.boardNode.innerHTML = '';
     for (var i = 0; i < 3; i++) {
       var row = document.createElement('tr');
-      row.className = `row-${i}`;
+      row.id = `row-${i}`;
       for (var j = 0; j < 3; j++) {
         var square = document.createElement('td');
-        square.className = `col-${j}`;
-        if (this.board[i][j] === 1) {
-          square.innerText = 'X';
-        } else if (this.board[i][j] === - 1) {
-          square.innerText = '0';
-        } else {
-          square.className = `col-${j} active`;
-        }
+        square.className = `${i}_${j} active`;
+        square.addEventListener('click', this.clickHandler)
         row.append(square);
       }
-      gameBoard.append(row);
+      this.boardNode.append(row);
     }
   }
 
-  toggle(row, col) {
-    if (this.playerOneTurn) {
-      this.board[row][col] = 1;
-      this.playerOneTurn = false;
-    } else {
-      this.board[row][col] = -1;
-      this.playerOneTurn = true;
+  /**
+   * @description handles DOM input, detecting active elements and parsing
+   * row and column data
+   * @param {Event} e click event
+   */
+  clickHandler(e) {
+    const divClass = e.target.classList;
+    if (divClass[1] === 'active') {
+      const [row, col] = divClass[0].split('_');
+      this.updateBoard(row, col, e.target);
+      this.checkBoard();
     }
-    this.displayBoard();
-    stylize();
-    this.checkBoard();
   }
 
+  /**
+   * @description updates board modal and dom
+   * @param {index} row 
+   * @param {index} col 
+   * @param {DOM Element} node
+   */
+  updateBoard(row, col, node) {
+    this.board[row][col] = this.playerOneTurn ? 1 : -1;
+    node.innerText = this.playerOneTurn ? 'X' : 'O';
+    node.className = `${row}_${col}`;
+  }
+
+  /**
+   * @description checks rows, columns and diagonals for 3 in a row
+   */
   checkBoard() {
     if (this.checkRows() || this.checkCols() || this.checkDiagonals()) {
-      var gameOver = document.createElement('h1');
-      gameOver.className = 'game-over';
-      gameOver.innerText = 'GAME OVER';
-      body.getElementsByTagName('header')[0].appendChild(gameOver);
-      document.getElementById('new-game').innerText = 'New Game?';
-      document.getElementById('new-game').addEventListener('click', () => {
-        var ttt = document.createElement('h1');
-        ttt.innerText = 'Tic Tac Toe';
-        body.getElementsByTagName('header')[0].innerHTML = '';
-        body.getElementsByTagName('header')[0].appendChild(ttt);
-        document.removeChild
-        newGame();
-      })
+      const winner = this.playerOneTurn ? 'ONE' : 'TWO';
+      this.gameOver(`PLAYER ${winner} VICTORY!`);
+    } else if (this.board.every(row => row.every(value => value !== null))) {
+      this.gameOver('DRAW');
     } else {
-      addClick(this.toggle);
+      this.playerOneTurn = !this.playerOneTurn;
     }
   }
 
@@ -101,9 +104,9 @@ class TicTacToeBoard {
     }
     if (total === 3 || total === -3) {
       return true;
-    } else {
-      total = 0;
     }
+    total = 0;
+
     for (var i = 0; i < this.board.length; i++) {
       total += this.board[i][2-i];
     }
@@ -113,32 +116,25 @@ class TicTacToeBoard {
     return false;
   }
 
+  gameOver(messageString) {
+    for (let div of document.getElementsByClassName('active')) {
+      div.classList = div.classList[0];
+    }
+    var message = document.createElement('h1');
+    message.innerText = messageString;
+    document.getElementById('header').appendChild(message);
+    document.getElementById('new_game').className = 'active';
+    document.getElementById('new_game').addEventListener('click', () => {
+      document.getElementById('header').removeChild(message);
+      document.getElementById('new_game').className = '';
+      this.resetBoard();
+    });
+  }
 }
 
-var stylize = () => { //make prettier
-  var squares = body.getElementsByTagName('td');
-  for (let square of squares) {
-    square.style.width = '50px';
-    square.style.height = '50px';
-    square.style.border = '1px dashed black';
-  }
-  document.getElementById('new-game').innerText = '';
-}
-var addClick = (callback) => {
-  var squares = body.getElementsByClassName('active');
-  for (let square of squares) {
-    square.addEventListener('click', (event) => {
-      var row = event.target.parentElement.className.slice(4,5);
-      var col = event.target.className.slice(4,5);
-      callback(row, col);
-    })
-  }
-}
 var newGame = () => {
   var board = new TicTacToeBoard();
-  board.displayBoard();
-  stylize();
-  addClick(board.toggle);
+  board.resetBoard();
 }
 
 newGame();
