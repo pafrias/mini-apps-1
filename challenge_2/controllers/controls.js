527c3cb040224709d7555a44916a067079045431
Pablo Frias <pafrias90@gmail.com>
2019-06-28 15:40:52 -0700

    Implement csv string generation
    Project still requires logic for file writing and reading, client
    rendering of the files in storage, data from the files on request,
    removal of last commas in csv files, and success page rendering.

--- challenge_2/controllers/controls.js
+++ challenge_2/controllers/controls.js
@@ -1,48 +1,57 @@
 const fs = require('fs');
 
-var cvsStringify = collection => {
+const {parse} = require('querystring')
+
+var csvStringify = collection => {
   var keys = ['firstName', 'lastName', 'county', 'city', 'role', 'sales'];
-  var results = [];
+  var headerString = 'firstName,lastName,county,city,role,sales\n';
+  function recurse(person) {
+    let results = '';
+    for (let key of keys) {
+      results += person[key] + ',';
+    }
+    results += '\n';
+    if (person.children.length) {
+      for (let child of person.children) {
+        results += recurse(child);
+      }
+    }
+    return results;
+  }
 
-  keys.map(key => {
-    results
-  })
-
-
+  return headerString += recurse(collection);
 }
 
-
-
-// fix later
 module.exports.read = (request, response) => {
-  // console.log('serving a GET request');
-  // fs.readFile('./samples/csv_report.csv', (err, fileData) => {
-  //   if (err) {
-  //     response.status(400); //
-  //     response.end();
-  //   } else {
-  //     var data = fileData;
-  //     response.status(200);
-  //     response.send(data);
-  //   }
-  // })
+  console.log('serving a GET request');
+  fs.readFile('./samples/csv_report.csv', (err, fileData) => {
+    if (err) {
+      // edit
+      response.status(400).end();
+    } else {
+      var data = fileData;
+      response.status(200).send(data);
+    }
+  })
 }
 
 module.exports.create = (request, response) => {
-  var data = request.body;
-  console.log('serving a POST request');
-  var defaultString = 'firstName,lastName,county,city,role,sales';
-  var cvsString = cvsStringify(data);
+  let buffer = '';
+  request.on('data', chunk => {
+    buffer += chunk.toString();
+  });
+  request.on('end', ()=> {
+    data = parse(buffer)['form-data'];
+    console.log(csvStringify(JSON.parse(data)));
+
+    response.status(200);//.send(csvStringify(JSON.parse(parse(buffer))));
+  })
+  //var csvString = cvsStringify(data);
   // write that data to file
   // send that file, and the string so that form can be filled on the other side
-  response.status(201).send(`<body><div>${arrayText}</div<p>you did it</p></body>`);
+  // response.status(201).send(`<body><div>${arrayText}</div<p>you did it</p></body>`);
 }
 
-var arrayText = [
-  'dogs',
-  'cats',
-  'otherthings'
-]
 var text = "firstName,lastName,county,city,role,sales\
 Joshie,Wyattson,San Mateo,San Mateo,Broker,1000000\
 Beth Jr.,Johnson,San Mateo,Pacifica,Manager,2900000\
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var _ = require('lodash');
 const PORT = 8080;
-const respond = require('./controllers/controls')
+const respond = require('./controllers/controls');
+const morgan = require('morgan');
 
 //var controls = require('./router/controls.js')
-
 var app = express();
-app.use(express.static('client/src/'));
-app.use(express.json());
+app.use(morgan('short'));
 app.get('/csv', respond.read);
 app.post('/csv', respond.create);
+app.use(express.static('client/src/'));
 
 app.listen(PORT, () => { console.log(`You are now listening on ${PORT}`)})
 
